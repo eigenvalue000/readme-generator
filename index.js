171e674f15d17b281ad3143af9b60b7819240c39
Garrett Kegel <ghkegel@gmail.com>
2021-08-21 21:55:12 -0700

    Deleted questionNames array since the array seemed redundant. The names of the questions will be in the name property of each prompt.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ inq = include('inquirer');
 gm = include('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = ['Project Title : ', 'Description : ', 'Table of Contents : ', 'Installation : ', 'Usage : ', 'License : ', 'Contributing : ', 'Tests : ', 'Questions : '];
-const questionNames = ['title', 'description', 'toc', 'installation', 'usage', 'license', 'contributing', 'tests', 'questions'];
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, function() {});
@@ -15,10 +15,10 @@ function init() {
     inq.prompt([
         {
             type: 'input',
-            name: questionNames[0],
+            name: 'title',
             message: questions[0],
         },{},{},{},{},{},{},{},{}])
-    .then((data) => writeToFile('README.md', data))
+    .then((data) => writeToFile('README.md', generateMarkdown(data)))
     .catch((err) => console.error(err));
 }
 
