489a279aedd4685dff10ec92efe6bc4f586fedf2
Garrett Kegel <ghkegel@gmail.com>
2021-08-21 22:07:31 -0700

    Added description, table of contents, and installation to readme generator prompt.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const questions = ['Project Title : ', 'Description : ', 'Table of Contents : ',
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, function() {});
+    fs.writeFile(fileName, data, function () { });
 }
 
 // TODO: Create a function to initialize app
@@ -17,9 +17,24 @@ function init() {
             type: 'input',
             name: 'title',
             message: questions[0],
-        },{},{},{},{},{},{},{},{}])
-    .then((data) => writeToFile('README.md', generateMarkdown(data)))
-    .catch((err) => console.error(err));
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: questions[1]
+        },
+    {
+        type: 'input',
+        name: 'tableOfContents',
+        message: questions[2],
+    },
+{
+    type: 'input',
+    name: 'installation',
+    message: questions[3],
+}])
+        .then((data) => writeToFile('README.md', gm(data)))
+        .catch((err) => console.error(err));
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -16,7 +16,7 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}, 
+  return `# ${data.title}, ${data.description}, ${data.tableOfContents}, ${data.installation}
 
 `;
 }
