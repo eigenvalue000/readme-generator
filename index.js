f71aec3d5e70ae1480150cfa192447b125026a7b
Garrett Kegel <ghkegel@gmail.com>
2021-08-21 21:22:23 -0700

    Added questions for prompt and a writeFile function.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // TODO: Include packages needed for this application
-
+fs = include('fs');
+inq = include('inquirer');
+gm = include('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['Project Title : ', 'Description : ', 'Table of Contents : ', 'Installation : ', 'Usage : ', 'License : ', 'Contributing : ', 'Tests : ', 'Questions : '];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function() {});
+}
 
 // TODO: Create a function to initialize app
 function init() {}
