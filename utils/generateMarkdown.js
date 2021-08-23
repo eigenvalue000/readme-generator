c1721d76fb2cc3492ac673acd427f4cbbcd0cd54
Garrett Kegel <ghkegel@gmail.com>
2021-08-22 22:19:36 -0700

    Added the Unlicense license.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,15 +7,15 @@ function renderLicenseBadge(license) {
   } else if (license == 'GNU GPLv3') {
     return '![GPL-3.0-or-later](https://img.shields.io/badge/license-GPL--3.0--or--later-blue)';
   } else if (license == 'Mozilla Public License 2.0') {
-    return '![MPL-2.0](https://img.shields.io/badge/license-MPL--2.0-brightgreen)'
+    return '![MPL-2.0](https://img.shields.io/badge/license-MPL--2.0-brightgreen)';
   } else if (license == 'Apache License 2.0') {
-    return '![Apache-2.0](https://img.shields.io/badge/license-Apache--2.0-yellowgreen)'
+    return '![Apache-2.0](https://img.shields.io/badge/license-Apache--2.0-yellowgreen)';
   } else if (license == 'MIT License') {
-    return '![MIT](https://img.shields.io/badge/license-MIT-yellow)'
+    return '![MIT](https://img.shields.io/badge/license-MIT-yellow)';
   } else if (license == 'Boost Software License 1.0') {
-    return '![BSL-1.0](https://img.shields.io/badge/license-BSL--1.0-9cf)'
-  } else if (license == 'Apache License 2.0') {
-    
+    return '![BSL-1.0](https://img.shields.io/badge/license-BSL--1.0-9cf)';
+  } else if (license == 'The Unlicense') {
+    return '![](https://img.shields.io/badge/license-Unlicense-blue)';
   }
 }
 
@@ -36,8 +36,8 @@ function renderLicenseLink(license) {
     return 'https://www.mit.edu/~amini/LICENSE.md';
   } else if (license == 'Boost Software License 1.0') {
     return 'https://www.boost.org/LICENSE_1_0.txt';
-  } else if (license == 'Apache License 2.0') {
-    
+  } else if (license == 'The Unlicense') {
+    return 'https://unlicense.org/';
   }
 }
 
@@ -135,8 +135,31 @@ SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE
 FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE,
 ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.`
-  } else if (license == 'Apache License 2.0') {
+  } else if (license == 'The Unlicense') {
+    return `This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
     
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+    
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+    
+For more information, please refer to <https://unlicense.org>`
   }
 }
 
